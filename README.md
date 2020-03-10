10d5a95ae674f260a10d2e2e02a966d9facfe659
scampion <scampion@macbook.local>
2020-03-10 20:40:39 +0100

    First release

--- README.md
+++ README.md
@@ -1,5 +1,72 @@
 Open GeoCoding
 --------------
+HTTP API Fuzzy match address resolution to longitude, latitude coordinate
 
 
 Based on open street map data, available at https://download.geofabrik.de/
+
+Example
+_______
+
+HTTP post on endpoint /resolve with header Content-Type: application/json : 
+
+```json
+{
+  "country":"belgium",
+  "postcode":"3630",
+  "city": "Maasmechelen",
+  "street": "Rijksweg",
+  "housenumber": "431"
+}
+```
+
+Result:
+
+
+```json
+{
+  "request": {
+    "city": "Maasmechelen",
+    "country": "belgium",
+    "housenumber": "431",
+    "postcode": "3630",
+    "street": "Rijksweg"
+  },
+  "response": [
+    [
+      5.6952999,
+      50.9601391,
+      "BE",
+      "3630",
+      "Maasmechelen",
+      "Rijksweg",
+      "431",
+      4.0
+    ]
+  ],
+  "time": 143.88608932495117
+}
+```
+
+
+Provisioning :
+______________
+
+This step download osm data, convert it in csv, create table in postgres and load it.
+
+Here a toy example :
+
+    docker-compose run ogc /app/ogc_import europe monaco
+
+/!\ params are used to build the following osm url : **https://download.geofabrik.de/$CONTINENT/$COUNTRY-latest.osm.pbf**
+
+
+Lauch on port 5000
+__________________
+
+    docker-compose up 
+
+
+You can use the script resolve to the it 
+
+    ./resolve tests/debug.json
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import time
 
 from flask import Flask, request, jsonify, g
-from psycopg2 import pool
+from psycopg2 import pool, sql
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('ogc')
 app.config['pool'] = pool.SimpleConnectionPool(1, 20, user=os.getenv('DATABASE_DB'),
                                                password=os.getenv('DATABASE_PASSWORD'),
@@ -24,13 +26,21 @@ def resolve():
     db = get_db()
     cur = db.cursor()
     q = request.get_json()
-    return jsonify({'request': q, 'response': {}})
-    #
-    # cur.execute("SELECT tablename, tableowner FROM pg_tables WHERE schemaname='public';")
-    # tables = list(cur.fetchall())
-    # for n, o in tables:
-    #     cur.execute("SELECT pg_size_pretty(pg_total_relation_size(%s));", (n,))
-    #     yield n, o, cur.fetchone()[0]
+    cur.execute("SELECT set_limit(0.2);")
+
+    s = "SELECT *,  " \
+        "similarity(city, %s) + similarity(postcode, %s) + similarity(street, %s) + similarity(housenumber, %s) AS similarity " \
+        "FROM {} WHERE city %% %s OR postcode %% %s OR street %% %s OR housenumber %% %s " \
+        "ORDER BY similarity DESC NULLS LAST LIMIT 1;"
+
+    query = sql.SQL(s).format(sql.Identifier(q['country']))
+    params = (
+    q['city'], q['postcode'], q['street'], q['housenumber'], q['city'], q['postcode'], q['street'], q['housenumber'])
+    start = time.time()
+    cur.execute(query, params)
+    end = time.time()
+    results = [r for r in cur.fetchall()]
+    return jsonify({'request': q, 'response': results, 'time': 1000 * (end - start)})
 
 
 if __name__ == '__main__':
